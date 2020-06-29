2f4bb7629169cdda863701d9ddc6e78b7968a5f4
rparkins999 <aleph0hpela-bughit@yahoo.co.uk>
2020-06-29 10:13:04 +0100

    Add support for variable depth backtrace to Llog.d

--- src/com/android/calendar/Llog.java
+++ src/com/android/calendar/Llog.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2020 Richard P. Parkins, M. A.
  *
+ * Logging with some backtrace information
  * Thanks to stackoverflow.com for hints on how to do it
  *
  * This file may be used under the terms of the GNU General Public License
@@ -12,26 +13,48 @@ package com.android.calendar;
 
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
 class Llog extends Object {
+    private static String getMethodName(StackTraceElement trace) {
+        String s = trace.getMethodName();
+        if (s.equals("<init>")) {
+            s = trace.getClassName();
+            s = s.substring(s.lastIndexOf(".") + 1);
+        }
+        return s;
+    }
+    private static String caller(int n, String args) {
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        int length = elements.length;
+        if (length < 5) { return ""; }
+        StackTraceElement trace = elements[4];
+        StringBuilder sb =
+            new StringBuilder(getMethodName(trace))
+                .append((args == null ? "()" : "(" + args + ")"))
+                .append(" line " + trace.getLineNumber());
+        for (int j = 5; j <= n + 4; ++j) {
+            if (j >= elements.length) { break; }
+            trace = Thread.currentThread().getStackTrace()[j];
+            sb.append(" called from ")
+              .append(getMethodName(trace))
+              .append(" line ")
+              .append(trace.getLineNumber());
+        }
+        return sb.toString();
+    }
     // Log method name with (), line number, and a string
     static void d(String s) {
-        StackTraceElement trace = Thread.currentThread().getStackTrace()[3];
-        String prefix = trace.getMethodName();
-        if (prefix.equals("<init>")) {
-            prefix = trace.getClassName();
-            prefix = prefix.substring(prefix.lastIndexOf(".") + 1);
-        }
-        Log.d(prefix + "()", "Line " + trace.getLineNumber() + " " + s);
+        Log.d(caller(0, null), s);
     }
     // Log method name with (args), line number, and a string
     // args should be stringified arguments to the method
     static void d(String s, String args) {
-        StackTraceElement trace = Thread.currentThread().getStackTrace()[3];
-        String prefix = trace.getMethodName();
-        if (prefix.equals("<init>")) {
-            prefix = trace.getClassName();
-            prefix = prefix.substring(prefix.lastIndexOf(".") + 1);
-        }
-        Log.d(prefix + "(" + args + ")", "Line " + trace.getLineNumber() + " " + s);
+        Log.d(caller(0, args), s);
+    }
+    // Log method name with n extra backtrace frames, and a string
+    // args should be stringified arguments to the method or null
+    static void d(String s, @Nullable String args, int n) {
+        Log.d(caller(n, args), s);
     }
 }
